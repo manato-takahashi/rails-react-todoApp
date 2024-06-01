b00190028da7d5468d0c1d770f88feb91a24e68e
manato-takahashi <manato.takahashi86@gmail.com>
2024-06-01 18:47:58 +0900

    add: タスク削除機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "tasks" => "tasks#index"
   post "tasks" => "tasks#create"
+  delete "tasks/:id" => "tasks#destroy"
 
   # Defines the root path route ("/")
   # root "posts#index"
